8166d4be3d23c8710b027e9c07a4ebf689fc389a
Bianca da Silva Campos <biancascampos0410@gmail.com>
2024-09-02 10:49:47 -0300

    organizing and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import Search from "./assets/svg/Search";
+import DisplayCityCard from "./components/cards/displayCityCard/displayCityCard";
 import Input from "./components/input/Input";
 import Title from "./components/title/Title";
 
@@ -8,14 +9,15 @@ const App = () => {
  
 
   return (
-    <div className="bg-gradient-to-b from-indigo-600 to-indigo-400 h-full w-full flex flex-col items-center">
-      <div className="lg:max-w-[30%]">
+    <div className="bg-gradient-to-b from-indigo-600 to-indigo-400 h-full  flex flex-col items-center">
+      <div className="lg:max-w-[40%] md:max-w-[40%] sm:max-w-[40%] w-full p-5" >
         <Title />
         <Input
           placeholder="Digite uma cidade"
           icon={<Search className=" size-7 " />}
         
         />
+        <DisplayCityCard/>
       </div>
     </div>
   );
