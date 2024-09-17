bc45bbe451a3d4282e702ddaebfda6dc46df67f9
Bianca da Silva Campos <biancascampos0410@gmail.com>
2024-09-17 08:55:59 -0300

    updating provider of city context

--- src/utils/providers/cityContextProvider.tsx
+++ src/utils/providers/cityContextProvider.tsx
@@ -4,9 +4,16 @@ import { City, CityContext } from "../context/cityContext";
 interface CityProviderProps {
   children: ReactNode;
 }
+const defaultCity: City = {
+  name: "",
+  state: "",
+  country: "",
+  lat: 0,
+  lon: 0,
+};
 
 export const CityProvider: React.FC<CityProviderProps> = ({ children }) => {
-  const [selectedCity, setSelectedCity] = useState<City | null>(null);
+  const [selectedCity, setSelectedCity] = useState<City | null>(defaultCity);
 
   return (
     <CityContext.Provider value={{ selectedCity, setSelectedCity }}>
