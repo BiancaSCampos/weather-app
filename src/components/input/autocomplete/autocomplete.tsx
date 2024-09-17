840c5a2bb8492ef510a6100137982885b8bc6fce
Bianca da Silva Campos <biancascampos0410@gmail.com>
2024-09-17 20:06:43 -0300

    removing comments

--- src/components/input/autocomplete/autocomplete.tsx
+++ src/components/input/autocomplete/autocomplete.tsx
@@ -58,10 +58,10 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
   };
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (filteredSuggestions.length === 0) return; // Evitar interações de teclado sem sugestões
+    if (filteredSuggestions.length === 0) return; 
 
     if (e.key === "Enter") {
-      e.preventDefault(); // Impedir o comportamento padrão do Enter no formulário
+      e.preventDefault(); 
       const selected = filteredSuggestions[activeSuggestionIndex];
       const formattedValue = `${selected.name}, ${selected.state || ""}, ${
         selected.country
@@ -72,11 +72,11 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
       setShowSuggestions(false);
       console.log("Selected city:", selected);
     } else if (e.key === "ArrowUp") {
-      e.preventDefault(); // Impedir o scrolling da página
+      e.preventDefault(); 
       if (activeSuggestionIndex === 0) return;
       setActiveSuggestionIndex(activeSuggestionIndex - 1);
     } else if (e.key === "ArrowDown") {
-      e.preventDefault(); // Impedir o scrolling da página
+      e.preventDefault(); 
       if (activeSuggestionIndex + 1 === filteredSuggestions.length) return;
       setActiveSuggestionIndex(activeSuggestionIndex + 1);
     }
