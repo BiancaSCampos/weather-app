034e996fbf7e079ad7b2a1c68840715a297c206a
Bianca da Silva Campos <biancascampos0410@gmail.com>
2024-09-17 20:41:23 -0300

    added the capitals constant file

---
+++ src/utils/constants/capitals.ts
@@ -0,0 +1,35 @@
+interface Capital {
+    latitude: number;
+    longitude: number;
+  }
+  
+ export const capitais: Record<string, Capital> = {
+    "Rio Branco": { latitude: -9.9754, longitude: -67.7792 },
+    "Maceió": { latitude: -9.6658, longitude: -35.7353 },
+    "Macapá": { latitude: 0.0343, longitude: -51.0708 },
+    "Manaus": { latitude: -3.119, longitude: -60.0216 },
+    "Salvador": { latitude: -12.9714, longitude: -38.5014 },
+    "Fortaleza": { latitude: -3.7172, longitude: -38.5433 },
+    "Vitória": { latitude: -20.3155, longitude: -40.3128 },
+    "Goiânia": { latitude: -16.6864, longitude: -49.2643 },
+    "São Luís": { latitude: -2.5489, longitude: -44.4141 },
+    "Cuiabá": { latitude: -15.5982, longitude: -56.0963 },
+    "Campo Grande": { latitude: -20.4697, longitude: -54.6201 },
+    "Belo Horizonte": { latitude: -19.8157, longitude: -43.9542 },
+    "Belém": { latitude: -1.4557, longitude: -48.4902 },
+    "João Pessoa": { latitude: -7.1151, longitude: -34.8641 },
+    "Curitiba": { latitude: -25.4284, longitude: -49.2733 },
+    "Recife": { latitude: -8.0476, longitude: -34.8776 },
+    "Teresina": { latitude: -5.0892, longitude: -42.8034 },
+    "Rio de Janeiro": { latitude: -22.9068, longitude: -43.1729 },
+    "Natal": { latitude: -5.7945, longitude: -35.2110 },
+    "Porto Alegre": { latitude: -30.0346, longitude: -51.2177 },
+    "Porto Velho": { latitude: -8.7608, longitude: -63.8991 },
+    "Boa Vista": { latitude: 2.8198, longitude: -60.6744 },
+    "Florianópolis": { latitude: -27.5954, longitude: -48.5480 },
+    "São Paulo": { latitude: -23.5505, longitude: -46.6333 },
+    "Aracaju": { latitude: -10.9472, longitude: -37.0731 },
+    "Palmas": { latitude: -10.1757, longitude: -48.2983 },
+    "Brasília": { latitude: -15.7801, longitude: -47.9292 },
+  };
+  
